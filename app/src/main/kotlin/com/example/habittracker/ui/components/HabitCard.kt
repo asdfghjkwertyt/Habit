5f743d6b35634302ab5fdf70d59b2cb476b6a505
Habit Tracker Developer <developer@habittracker.com>
2026-03-23 15:11:03 +0530

    Import RoundedCornerShape in HabitCard

--- app/src/main/kotlin/com/example/habittracker/ui/components/HabitCard.kt
+++ app/src/main/kotlin/com/example/habittracker/ui/components/HabitCard.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Checkbox
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
